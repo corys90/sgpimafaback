1eb1a48b3f2c8e06618fa1531b9bdbd3c3175e50
Cristian A <corys90@hotmail.com>
2024-09-27 23:18:47 -0500

    update: nueva actualización de password en login 2

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -14,7 +14,7 @@ import * as env from '../../env';
 const PageLogin = () => {
 
     const dispatch = useDispatch();
-    let [formData, setFormData] = useState({user:"imafasas", pass:"prueba", valid: "login"});
+    let [formData, setFormData] = useState({user:"", pass:"", valid: "login"});
     const navigate = useNavigate();
     const [msgDlgShow, setMsgDlgShow] = useState(false);
     const [msgAlrtUsr, setMsgAlrtUsr] = useState(false);
